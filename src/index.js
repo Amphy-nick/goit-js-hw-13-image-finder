16cdba5744b8a5fb397ff810de11e4658c06eee4
Burkovskyi Dmytro <79474730+Amphy-nick@users.noreply.github.com>
2021-09-04 19:59:31 +0300

    Написал базовую логику, добавил плагины
    Написал базовую логику, связал с разметкой, вынес функцию fetch в отдельный документ, добавил плагины pnotify и material icons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import './sass/main.scss';
+import NewsApiService from './js/apiFetch';
+import markup from './templates/markup.hbs'
+import { alert, notice, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    articlesContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
+}
+
+const newsApiService = new NewsApiService(); 
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+   e.preventDefault();
+
+     newsApiService.query = e.currentTarget.elements.query.value ;
+
+     if(e.currentTarget.elements.query.value === '' 
+     || e.currentTarget.elements.query.value.length < 2 ) {
+         return error('test')
+     }
+
+     newsApiService.resetPage();
+     newsApiService.fetchArticles().then(hits =>{
+        clearForm() 
+        appendHitsMarkup(hits)
+    })
+}
+
+function onLoadMore() {
+
+    newsApiService.fetchArticles().then(appendHitsMarkup)
+    
+}
+
+function appendHitsMarkup(hits) {
+    refs.articlesContainer.insertAdjacentHTML('beforeend', markup(hits))
+}
+
+function clearForm() {
+    refs.articlesContainer.innerHTML = '';
+}
---
+++ src/js/apiFetch.js
@@ -0,0 +1,42 @@
+const KEY = 'key=23206361-11fddcc9c6397c9794392b9fa'
+const BASE_URL = 'https://pixabay.com/api/?'
+
+export default class NewsApiService {
+constructor(){
+    this.searchQuery = '';
+    this.page = 1;
+}
+
+fetchArticles(){
+console.log(this)
+    
+const url = `${BASE_URL}${KEY}&q=${this.searchQuery}&
+image_type=photo&per_page=5&page=${this.page}`
+
+return fetch(url)
+.then(response => response.json())
+.then(({hits}) => {
+console.log(hits)
+    this.incrementPage();
+
+    return hits;
+})
+}
+
+incrementPage() {
+    this.page +=1
+}
+
+resetPage() {
+    this.page = 1
+}
+
+get query() {
+    return this.searchQuery;
+}
+
+set query(newQuery) {
+    this.searchQuery = newQuery;
+}
+}
+
