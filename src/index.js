21484cf793151c1d4cdf58d33f6c015b2ca35bba
Burkovskyi Dmytro <79474730+Amphy-nick@users.noreply.github.com>
2021-09-06 21:10:56 +0300

    Сделал дз
    1)Добавил плагин бэйсиклайт, вынес его в отдельный файл. Модалка работает
    2)Изменил стилизацию некоторых элементов галереи;
    3)Добавил скроллвью
    4)Придумал, как скрыть кнопку "искать еще"

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import './sass/main.scss';
 import NewsApiService from './js/apiFetch';
-import markup from './templates/markup.hbs'
-import { alert, notice, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
-import '@pnotify/core/dist/BrightTheme.css';
-import * as basicLightbox from 'basiclightbox'
+import refs from './js/refs'
+import onOpenModal from './js/modal'
 
-const refs = {
-    searchForm: document.querySelector('.search-form'),
-    articlesContainer: document.querySelector('.gallery'),
-    loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
-}
+import markup from './templates/markup.hbs'
+
+import { error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const newsApiService = new NewsApiService(); 
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.articlesContainer.addEventListener('click', onOpenModal);
 
 function onSearch(e) {
    e.preventDefault();
@@ -27,7 +25,7 @@ function onSearch(e) {
      }
      
      newsApiService.resetPage();
-     newsApiService.fetchArticles().then(hits =>{
+     newsApiService.fetchArticles().then(hits => {
         clearForm() 
         appendHitsMarkup(hits)
     })
@@ -35,8 +33,14 @@ function onSearch(e) {
 }
 
 function onLoadMore() {
-    newsApiService.fetchArticles().then(appendHitsMarkup)
-}
+    newsApiService.fetchArticles().then(appendHitsMarkup).then(() => {
+    
+    refs.loadMoreBtn.scrollIntoView({
+    block: "start",
+    behavior: "smooth"
+});
+    })
+};
 
 function appendHitsMarkup(hits) {
     refs.articlesContainer.insertAdjacentHTML('beforeend', markup(hits))
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+    searchForm: document.querySelector('.search-form'),
+    articlesContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
+}
